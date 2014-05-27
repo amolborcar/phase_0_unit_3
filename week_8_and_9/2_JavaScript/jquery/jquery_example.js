d67f3cac87db7cb3385926bee4ae610275d66881
Amol Borcar <aborcar@gmail.com>
2014-05-27 13:28:12 -0700

    added jquery challenge solution

--- week_8_and_9/2_JavaScript/jquery/jquery_example.js
+++ week_8_and_9/2_JavaScript/jquery/jquery_example.js
@@ -7,21 +7,38 @@ $('body').css({'background-color': 'pink'})
  
 //RELEASE 1:
   //Add code here to select elements of the DOM 
+
+  var bodyElement = $('body')
  
  
 //RELEASE 2: 
   // Add code here to modify the css and html of DOM elements
+
+  $('h1').css({'color': 'blue', 'border': 'black 1px solid', 'visibility': 'visible'})
+  $('img').css({'border': 'black 1px solid'})
+  $('.mascot h1').html("Chorus Frogs")
  
  
 //RELEASE 3: Event Listener
   // Add the code for the event listener here 
+
+  $('img').on('mouseover', function(event){
+  	event.preventDefault()
+  	$(this).attr('src', 'http://gmkfreelogos.com/logos/W/img/Warner_Bros_Michigan_J_Frog.gif')
+  });
  
  
 //RELEASE 4 : Experiment on your own
- 
- 
- 
- 
- 
+
+	$('.myImage').click(function() {
+		$('img').animate({
+			opacity: 0.8,
+			height: 500,
+			borderWidth: 10,
+			marginLeft: 300
+			},
+			1000, function() {
+		})
+	});	
  
 })  // end of the document.ready function: do not remove or write DOM manipulation below this.
