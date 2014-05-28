432e0aa47c0b839fb06c61bed0595380168cf0e4
Amol Borcar <aborcar@gmail.com>
2014-05-28 14:16:06 -0700

    finished javascript gradebook challenge

--- week_8_and_9/2_JavaScript/gradebook/my_solution.js
+++ week_8_and_9/2_JavaScript/gradebook/my_solution.js
@@ -29,14 +29,14 @@ var scores = [ [80, 70, 70, 100],
 // Create a variable Average and assign it the value of a function
 // Have average accept an array of numbers and returns the average of those numbers
 
-var average = function(arr) {
-  var total = 0;
-  for (var i in arr) {
-    total += arr[i];
-  }
-  var avg = total/arr.length;
-  return avg;
-}
+// var average = function(arr) {
+//   var total = 0;
+//   for (var i in arr) {
+//     total += arr[i];
+//   }
+//   var avg = total/arr.length;
+//   return avg;
+// }
 
 // Create a variable called gradebook and assign it a value of a new object
 // Make each student name in students a property of gradebook and assign each the value of a new object.
@@ -55,7 +55,7 @@ var gradebook = {
     name: "Elizabeth"
   }
 
-}
+};
 
 // Give each student property of gradebook its own testScores property and assign it the value of the respective students scores from scores.
 
@@ -66,42 +66,67 @@ gradebook.Elizabeth.testScores = scores[3];
 
 // create an addscore property on gradebook that adds a score to a person's array
 
-gradebook.addScore = function(student, score) {
-  gradebook.student.testScores.push(score);
-}
+// gradebook.addScore = function(student, score) {
+//   student.testScores.push(score);
+// }
 
 // A getAverage property to gradebook and assign it the value of a function ...
 // Modify getAverage so that it accepts a name as a String (e.g., "Joseph") and returns the named students average.
 
-gradebook.getAverage = function(name) {
-  for (var i in gradebook) {
-    if (this.i.name === name) {
-      return this.average(this.i.testScores);
-    }
-  }
-}
+// gradebook.getAverage = function(name) {
+//   for (var i in gradebook) {
+//     if (gradebook[i].hasOwnProperty("name")) {
+//       if (gradebook[i]["name"] === name) {
+//         return average(gradebook[i]["testScores"]);
+//       }
+//     }
+//   }
+// }
 
+// for (var i in gradebook) {
+//   if (gradebook[i].hasOwnProperty("name")) {
+//     console.log(gradebook[i]["name"]);
+//   } else {
+//     console.log("no name");
+//   }
+// }
 
 
 // __________________________________________
 // Refactored Solution
 
+var average = function(arr) {
+  var total = 0;
+  for (var i in arr) {
+    total += arr[i];
+  }
+  var avg = total/arr.length;
+  return avg;
+}
 
+gradebook.addScore = function(student, score) {
+  student.testScores.push(score);
+}
 
-
-
-
+// Removed hasOwnProperty line, it was unnecessary
+gradebook.getAverage = function(name) {
+  for (var i in gradebook) {
+    if (gradebook[i]["name"] === name) {
+      return average(gradebook[i]["testScores"]);
+    }
+  }
+}
 
 
 // __________________________________________
 // Reflect
 
-
-
-
-
-
-
+// This challenge ended up being a lot more difficult than I thought it would be originally.
+// The thing that kept throwing me was supplying the student's name as a string, as opposed to
+// as the name of the object, and having the function find the right student.  I think my method
+// was convoluted but I tried to do it without looking up anything online, and it worked, so I was
+// glad I could solve it on my own.  However, I am going to go lookup other methods that may
+// be more efficient or easier to understand.
 
 
 // __________________________________________
@@ -147,28 +172,26 @@ assert(
   "5. "
 )
 
-// assert(
-//   (gradebook.addScore instanceof Function),
-//   "The value of gradebook's addScore property should be a Function.",
-//   "6. "
-// )
+assert(
+  (gradebook.addScore instanceof Function),
+  "The value of gradebook's addScore property should be a Function.",
+  "6. "
+)
 
-// gradebook.addScore("Susan", 80)
+gradebook.addScore(gradebook.Susan, 80);
 
-// assert(
-//   (gradebook.Susan.testScores.length === 5
-//    && gradebook.Susan.testScores[4] === 80),
-//   "Susan's testScores should have a new score of 80 added to the end.",
-//   "7. "
-// )
+assert(
+  (gradebook.Susan.testScores.length === 5
+   && gradebook.Susan.testScores[4] === 80),
+  "Susan's testScores should have a new score of 80 added to the end.",
+  "7. "
+)
 
 assert(
   (gradebook.getAverage instanceof Function),
   "The value of gradebook's getAverage property should be a Function.",
   "8. "
 )
-
-gradebook.getAverage("Joseph");
 
 assert(
   (gradebook.getAverage("Joseph") === 80),
